70b45aa324404850ff28b8f37e7f6341c3769643
Suhas Patil <suhas.patil@eficode.com>
2025-11-05 00:53:57 +0100

    adding tf validate

--- terraform-aws-s3-demo/main.tf
+++ terraform-aws-s3-demo/main.tf
@@ -21,7 +21,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my_demo_bucket" {
   bucket = "suhas-github-actions-demo-bucket-2025-07-26-xyz12"
-#  acl    = "private"  validation error
+  #  acl    = "private"  validation error
   tags = {
     Name        = "GitHubActionsDemoBucket1"
     Environment = "Dev"
