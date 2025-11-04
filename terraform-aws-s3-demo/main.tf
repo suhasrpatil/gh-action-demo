c587389907e0178961ed67acc2db9c7219b40819
Suhas Patil <suhas.patil@eficode.com>
2025-11-05 00:26:03 +0100

    tf validate 7

--- terraform-aws-s3-demo/main.tf
+++ terraform-aws-s3-demo/main.tf
@@ -42,7 +42,7 @@ resource "aws_s3_bucket_public_access_block" "my_demo_bucket_public_access_block
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-
+}
 
 
 output "bucket_name" {
