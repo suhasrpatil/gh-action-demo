eb8bd0105b8a5533d0c036fcc5b6fd99a3151fd0
Suhas Patil <suhas.patil@eficode.com>
2025-11-05 00:38:00 +0100

    tf validate 1

--- terraform-aws-s3-demo/main.tf
+++ terraform-aws-s3-demo/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "suhas-ci-cd-terraform-state-bucket"
-    key            = "github-actions-demo/terraform.tfstate"
+      key            = "github-actions-demo/terraform.tfstate"
     region         = "eu-north-1"
     encrypt        = true
     dynamodb_table = "demo-terraform-state-locking"
@@ -21,6 +21,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my_demo_bucket" {
   bucket = "suhas-github-actions-demo-bucket-2025-07-26-xyz12"
+  acl = "private"
   tags = {
     Name        = "GitHubActionsDemoBucket1"
     Environment = "Dev"
